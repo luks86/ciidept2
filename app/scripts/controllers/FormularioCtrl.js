8305b712935ad24e658c736d2514fc748d755500
Luk <Luk>
2016-09-09 19:01:50 -0300

    form_controller_correccion

--- app/scripts/controllers/FormularioCtrl.js
+++ app/scripts/controllers/FormularioCtrl.js
@@ -161,9 +161,110 @@ $scope.mData.destino = correo;
     .controller('FormularioCtrl', FormularioCtrl);
 
 
+
+
 function FormularioCtrl($scope, $http)
 {
   
+  'use strict';
+
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function() {
+    $scope.dt = null;
+  };
+
+  $scope.inlineOptions = {
+    customClass: getDayClass,
+    minDate: new Date(),
+    showWeeks: true
+  };
+
+  $scope.dateOptions = {
+    dateDisabled: disabled,
+    formatYear: 'yy',
+    maxDate: new Date(2020, 5, 22),
+    minDate: new Date(),
+    startingDay: 1
+  };
+
+  // Disable weekend selection
+  function disabled(data) {
+    var date = data.date,
+      mode = data.mode;
+    return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+  }
+
+  $scope.toggleMin = function() {
+    $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
+    $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
+  };
+
+  $scope.toggleMin();
+
+  $scope.open1 = function() {
+    $scope.popup1.opened = true;
+  };
+
+  $scope.open2 = function() {
+    $scope.popup2.opened = true;
+  };
+
+  $scope.setDate = function(year, month, day) {
+    $scope.dt = new Date(year, month, day);
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+  $scope.altInputFormats = ['M!/d!/yyyy'];
+
+  $scope.popup1 = {
+    opened: false
+  };
+
+  $scope.popup2 = {
+    opened: false
+  };
+
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  var afterTomorrow = new Date();
+  afterTomorrow.setDate(tomorrow.getDate() + 1);
+  $scope.events = [
+    {
+      date: tomorrow,
+      status: 'full'
+    },
+    {
+      date: afterTomorrow,
+      status: 'partially'
+    }
+  ];
+
+  function getDayClass(data) {
+    var date = data.date,
+      mode = data.mode;
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+      for (var i = 0; i < $scope.events.length; i++) {
+        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+        if (dayToCheck === currentDay) {
+          return $scope.events[i].status;
+        }
+      }
+    }
+
+    return '';
+  }
+
+
+
+
   $scope.DatosLogeo = {email:'juan41285@gmail.com',password:'@piTIC2016'};
 
   $scope.user = {};
@@ -181,17 +282,53 @@ function FormularioCtrl($scope, $http)
 
                 .success(function(data){
                 console.log(data);
-                
+/*                $scope.user={fecha:$scope.dt}
 
+*/                
+                /*console.log($scope.dt);*/
+                console.log($scope.user.fecha);
+
+                $scope.user={
+                                entrada:$scope.user.entrada,  
+                                salida:$scope.user.salida,
+                                evento:$scope.user.evento,
+                                organizador:$scope.user.organizador,
+                                fecha:$scope.dt, 
+                                apellidonombre:$scope.user.apellidonombre,
+                                asistentes:$scope.user.asistentes,
+                                catering:$scope.user.catering,
+                                email:$scope.user.email,
+                                telefono:$scope.user.telefono,
+                                objetivos:$scope.user.objetivos,
+                                destinatarios:$scope.user.destinatarios,
+                                };
+
+                
+                /*$scope.user={
+                                entrada:"10:00",  
+                                salida:"12:00",
+                                evento:"Evento Prueba",
+                                organizador:"Evento Prueba",
+                                fecha:"2016-09-08", 
+                                apellidonombre:"ape1lidonombre",
+                                asistentes:"30",
+                                catering:"0",
+                                email:"lucas.laime86@gmail.com",
+                                telefono:"155580098",
+                                objetivos:"Hola soy un objetivo",
+                                destinatarios:"Hola soy los destinatarios",
+                                };
+*/
                     $http({
                       method  : 'POST',
-                      url     : 'http://agenda.innovacioneducativa.gob.ar/api/v1/agenda?token='+ $data.token,
+                      url     : 'http://agenda.innovacioneducativa.gob.ar/api/v1/agenda?token='+ data.token,
                       data    :/*{email: vm.email, password: vm.password}*/$.param($scope.user),  // pass in data as strings
                       headers : { 'Content-Type': 'application/x-www-form-urlencoded' }  // set the headers so angular passing info as form data (not request payload)
                     })
 
                     .success(function(data){
                     console.log(data);
+
                     });  
             });
 
